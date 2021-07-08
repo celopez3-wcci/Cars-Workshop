9be59ccbcd31d6d0ba2b031af99725c029e31d00
Carlos Lopez <79668342+celopez3-wcci@users.noreply.github.com>
2021-07-08 19:08:46 +0000

    created changes for iteration 1 and 2 of the cars workshop

--- scripts/Car.js
+++ scripts/Car.js
@@ -1,35 +1,57 @@
 class Car {
-    constructor() {
-        this._speed = 0;
-        this._engineHealth = 100;
+    constructor(){
+      this.speed = 0;
+      this.engineHealth = 100;
+      this.frictionIntervalID = undefined;
     }
 
-    accelerate() {
-        this._speed += 10;
-        if (this._speed > 60) {
-            this._engineHealth -= this._speed - 60;
-            if (this._engineHealth < 0) {
-                this._engineHealth = 0;
-            }
+    startFriction(){
+      if(this.frictionIntervalID == undefined){
+        this.frictionIntervalID = self.setInterval(this.slowDown, 1000);
+      }
+    }
+
+    startCruiseControl(){
+      clearInterval(this.frictionIntervalID);
+      this.frictionIntervalID = undefined;
+    }
+
+    accelerate(){
+      if(this.speed >= 170){
+        this.speed = 170;
+      }else{
+        this.speed+=10;
+        if(this.speed >= 130){
+          this.engineHealth-=50;
         }
+      }
     }
 
-    brake() {
-        this._speed -= 10;
-        if (this._speed < 0) {
-            this._speed = 0;
-        }
-    }
-    reset(){
-        this._speed = 0;
-        this._engineHealth = 100;
+    slowDown = () => {
+      if(this.speed > 0){
+        this.speed-=1;
+      }
     }
 
-    getEngineHealth() {
-        return this._engineHealth;
+    brake(){
+      if(this.speed <= 0){
+        this.speed = 0;
+      }else{
+        this.speed-=10;
+      }
+
+      if(this.frictionIntervalID == undefined){
+        this.startFriction();
+      }
     }
 
-    getSpeed() {
-        return this._speed;
+
+    getSpeed(){
+      return this.speed;
     }
+
+    getEngineHealth(){
+      return this.engineHealth;
+    }
+
 }
--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,35 @@
+const goFastBtn = document.getElementsByClassName("floorboard__accelerator")[0];
 
+const stopBtn = document.getElementsByClassName("floorboard__brake")[0];
+
+const cruiseBtn = document.getElementsByClassName("floorboard__cruise")[0];
+
+const checkEngineLight = document.getElementsByClassName("dashboard__check-engine-light")[0];
+
+const speedometerDiv = document.getElementById("speedometer");
+
+let refreshIntervalID = setInterval(updateDashboard, 1000);
+
+const myCar = new Car();
+myCar.startFriction();
+
+goFastBtn.addEventListener("click", function(){
+ myCar.accelerate();
+ //speedometerDiv.innerText = myCar.getSpeed();
+ if(myCar.getEngineHealth() <= 50){
+   checkEngineLight.innerText = "Check Engine";
+ }
+});
+
+stopBtn.addEventListener("click", function(){
+  myCar.brake();
+  //speedometerDiv.innerText = myCar.getSpeed();
+});
+
+cruiseBtn.addEventListener("click", function(){
+  myCar.startCruiseControl();
+});
+
+function updateDashboard(){
+  speedometerDiv.innerText = myCar.getSpeed();
+}
